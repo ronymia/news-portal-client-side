e4b0c192faf6b048f195cf3808ced0354186b589
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-01-26 15:22:58 +0600

    category base show data

--- src/Pages/Category/Category.js
+++ src/Pages/Category/Category.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import NewsSummaryCard from '../shared/NewsSummaryCard/NewsSummaryCard';
 
 const Category = () => {
     const categoryNews = useLoaderData();
 
     return (
-        <div>
-            <h1>this is category news {categoryNews.length}</h1>
-        </div>
+        <section>
+            {
+                categoryNews.map(news => <NewsSummaryCard
+                    key={news._id}
+                    news={news}
+                ></NewsSummaryCard>)
+            }
+        </section>
     )
 }
 
