752471e22b562d55e55e7004059e3f48d4d97a66
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-01-26 12:46:14 +0600

    Responsive Navbar

--- src/Pages/shared/Header/Header.js
+++ src/Pages/shared/Header/Header.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import LeftSideNav from '../LeftSideNav/LeftSideNav';
+import RightSideNav from '../RightSideNav/RightSideNav';
 
 const Header = () => {
     return (
-        <div>
-            <h1>this is header</h1>
-        </div>
+        <Navbar collapseOnSelect className='mb-4' expand="lg" bg="light" variant="light">
+            <Container>
+                <Navbar.Brand><Link to='/'>News portal</Link></Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#features">All News</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#login">Login</Nav.Link>
+                    </Nav>
+                    <div className='d-lg-none'>
+                        <LeftSideNav></LeftSideNav>
+                        <RightSideNav></RightSideNav>
+                    </div>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar >
     )
 }
 
