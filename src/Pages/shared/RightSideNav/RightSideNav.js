5795116e81708f201f494a9c581754025a0a6f78
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-01-26 12:20:29 +0600

    right side nab with brand carousel

--- src/Pages/shared/RightSideNav/RightSideNav.js
+++ src/Pages/shared/RightSideNav/RightSideNav.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaWhatsapp, FaDiscord, FaTwitch } from "react-icons/fa";
+import ListGroup from 'react-bootstrap/ListGroup';
+import BrandCarousel from '../BrandCarousel/BrandCarousel';
 
 const RightSideNav = () => {
     return (
-        <div>
-            <h1>this is right side navbar</h1>
-        </div>
+        <section>
+            <ButtonGroup vertical className="w-100">
+                <Button className='mb-2 w-full' variant="outline-primary">
+                    <FaGoogle></FaGoogle> Login with Google
+                </Button>
+                <Button variant="outline-dark">
+                    <FaGithub></FaGithub> Login with Github
+                </Button>
+            </ButtonGroup>
+            <div className="mt-2">
+                <h5>Find us on</h5>
+                <ListGroup>
+                    <ListGroup.Item className="mb-2"><FaFacebook /> Facebook</ListGroup.Item>
+                    <ListGroup.Item className="mb-2"><FaWhatsapp /> What's app</ListGroup.Item>
+                    <ListGroup.Item className="mb-2"><FaTwitter /> Twitter</ListGroup.Item>
+                    <ListGroup.Item className="mb-2"><FaDiscord /> Discord</ListGroup.Item>
+                    <ListGroup.Item className="mb-2"><FaTwitch /> Privacy policy</ListGroup.Item>
+                </ListGroup>
+            </div>
+            <div className="mt-2">
+                <BrandCarousel />
+            </div>
+        </section>
     )
 }
 
